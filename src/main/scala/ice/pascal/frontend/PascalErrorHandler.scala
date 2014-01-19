0c0a3bafe3ed40148d625098d236731dfb3b200f
John <jfourkiotis@gmail.com>
2014-01-19 17:26:49 +0200

    error handling in Pascal

---
+++ src/main/scala/ice/pascal/frontend/PascalErrorHandler.scala
@@ -0,0 +1,27 @@
+package ice.pascal.frontend
+
+import ice.frontend.{Token, Parser}
+import ice.message._
+
+object PascalErrorHandler {
+  private val MAX_ERRORS = 25
+  private var errorCount_ = 0 // syntax errors
+  
+  def flag(token: Token, errorCode: PascalErrorCode, parser: Parser) {
+    val m = new Message(MessageType.SYNTAX_ERROR, (token.position, token.lexeme, errorCode.toString))
+    parser.sendMessage(m)
+    
+    // abort if too many errors
+    errorCount_ += 1
+    if (errorCount_ > MAX_ERRORS) {
+      import error.TOO_MANY_ERRORS
+      abortTranslation(TOO_MANY_ERRORS, parser)
+    }
+  }
+  
+  def abortTranslation(errorCode: PascalErrorCode, parser: Parser) {
+    val fatalText = "FATAL ERROR: " + errorCode.toString
+    parser.sendMessage(new Message(MessageType.SYNTAX_ERROR, (0, 0, "", fatalText)))
+    System.exit(errorCode.status)
+  }
+}
