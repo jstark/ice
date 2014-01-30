9b9ccac98c46acc94a5c1799e754778771f8506c
John <jfourkiotis@gmail.com>
2014-01-30 22:14:51 +0200

    do-while replaced with while loop

--- src/main/scala/ice/pascal/frontend/PascalParser.scala
+++ src/main/scala/ice/pascal/frontend/PascalParser.scala
@@ -4,27 +4,29 @@ import ice.frontend.{Token, Parser, EofToken}
 import ice.message.{Message, MessageType}
 
 class PascalParser(scanner: PascalScanner) extends Parser(scanner) {
+
+  tokens.init()
   
   def parse() = {
     try
     {
-    val start = System.currentTimeMillis 
-    var token: Token = null
-    do {
-      token = nextToken()
-      
-      if (token.ttype != ERROR) {
-        val args = (token.lineNumber, token.position, token.ttype, token.lexeme, token.tvalue)
-        val m = new Message(MessageType.TOKEN, args)
-        sendMessage(m)
-      } else {
-        PascalErrorHandler.flag(token, token.tvalue.asInstanceOf[PascalErrorCode], this)
+      val start = System.currentTimeMillis
+      var token = nextToken()
+      while (!token.isInstanceOf[EofToken]) {
+        if (token.ttype != tokens.ERROR) {
+          val args = (token.lineNumber, token.position, token.ttype, token.lexeme, token.tvalue)
+          val m = new Message(MessageType.TOKEN, args)
+          sendMessage(m)
+        } else {
+          PascalErrorHandler.flag(token, token.tvalue.asInstanceOf[PascalErrorCode], this)
+        }
+        token = nextToken()
       }
-    } while (!token.isInstanceOf[EofToken])
-    
-    val elapsedTime = (System.currentTimeMillis - start)/1000.0f
-    val m = new Message(MessageType.PARSER_SUMMARY, (token.lineNumber, errorCount, elapsedTime))
-    sendMessage(m)
+
+      val elapsedTime = (System.currentTimeMillis - start)/1000.0f
+      val m = new Message(MessageType.PARSER_SUMMARY, (token.lineNumber, errorCount, elapsedTime))
+      sendMessage(m)
+
     } catch {
       case _: Throwable => PascalErrorHandler.abortTranslation(error.IO_ERROR, this)
     }
