073292750b94b5bfee07f1e6378953d038ada790
John <jfourkiotis@gmail.com>
2014-01-31 23:31:59 +0200

    A Parser has a SymTabStack

--- src/main/scala/ice/frontend/Parser.scala
+++ src/main/scala/ice/frontend/Parser.scala
@@ -4,7 +4,8 @@ import ice.message.MessageProducer
 import ice.intermediate._
 
 abstract class Parser(scanner: Scanner) extends MessageProducer {
-  def parse(): (ICode, SymbolTable)
+  val symTabStack = SymTab.createStack()
+  def parse(): (ICode, SymTab)
   def errorCount: Int
   def currentToken = scanner.currentToken
   def nextToken() = scanner.nextToken
