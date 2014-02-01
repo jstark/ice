0c1f8419e4a8c1949cadc5ce3b615c7d3a0d9cb1
John <jfourkiotis@gmail.com>
2014-02-01 17:49:28 +0200

    A parser.parse method returns a SymbolTableStack

--- src/main/scala/ice/backend/Backend.scala
+++ src/main/scala/ice/backend/Backend.scala
@@ -1,18 +1,18 @@
 package ice.backend
 
 import ice.message._
-import ice.intermediate.SymTab
+import ice.intermediate.SymTabStack
 import ice.intermediate.ICode
 
 abstract class Backend extends MessageProducer {
-  def process(icode: ICode, symbolTable: SymTab)
+  def process(icode: ICode, symTabStack: SymTabStack)
 }
 
 /*
  * compiler
  */
 class CodeGenerator extends Backend {
-  def process(icode: ICode, symbolTable: SymTab) {
+  def process(icode: ICode, symbolTabStack: SymTabStack) {
     val start = System.currentTimeMillis
     val elapsedTime = (System.currentTimeMillis - start)/1000.0f
     val instructionCount = 0
@@ -26,7 +26,7 @@ class CodeGenerator extends Backend {
  * interpreter
  */
 class Executor extends Backend {
-  def process(icode: ICode, symbolTable: SymTab) {
+  def process(icode: ICode, symTabStack: SymTabStack) {
     val start = System.currentTimeMillis
     val elapsedTime = (System.currentTimeMillis - start)/1000.0f
     val executionCount = 0
--- src/main/scala/ice/frontend/Parser.scala
+++ src/main/scala/ice/frontend/Parser.scala
@@ -5,7 +5,7 @@ import ice.intermediate._
 
 abstract class Parser(scanner: Scanner) extends MessageProducer {
   val symTabStack = SymTab.createStack()
-  def parse(): (ICode, SymTab)
+  def parse(): (ICode, SymTabStack)
   def errorCount: Int
   def currentToken = scanner.currentToken
   def nextToken() = scanner.nextToken
