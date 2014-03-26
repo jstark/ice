95affc0e354ebb4992d378a625bc2eb0a17e3dfc
John <jfourkiotis@gmail.com>
2014-03-26 19:25:55 +0200

    Changes to handle proper parsing

--- src/main/scala/ice/pascal/frontend/PascalParser.scala
+++ src/main/scala/ice/pascal/frontend/PascalParser.scala
@@ -2,6 +2,7 @@ package ice.pascal.frontend
 
 import ice.frontend.{Token, Parser, EofToken}
 import ice.message.{Message, MessageType}
+import ice.intermediate.{ICodeNode, ICode, SymTab}
 
 class PascalParser(scanner: PascalScanner) extends Parser(scanner) {
 
@@ -10,27 +11,30 @@ class PascalParser(scanner: PascalScanner) extends Parser(scanner) {
   def errorCount = PascalErrorHandler.errorCount
   
   def parse() = {
+    val symtabstack = SymTab.createStack()
+    val icode = ICode.createICode()
+    val start = System.currentTimeMillis
     try
     {
-      val start = System.currentTimeMillis
       var token = nextToken()
-      while (!token.isInstanceOf[EofToken]) {
-        if (token.ttype == tokens.IDENTIFIER) {
-          val name = token.lexeme.toLowerCase()
+      var root: ICodeNode = null;
+      // look for the BEGIN token to parse a compound statement
+      if (token.ttype == tokens.BEGIN) {
+        val stmnt_parser = StatementParser(scanner, symtabstack, listeners)
+        root = stmnt_parser.parse(token)
+      } else {
+        PascalErrorHandler.flag(token, error.UNEXPECTED_TOKEN, this)
+      }
 
-          // if it's not already in the symbol table,
-          // create and enter a new entry for the identifier.
-          var entry = symTabStack.lookup(name)
-          if (entry == null) {
-            entry = symTabStack.enterLocal(name)
-          }
+      // look for the final period.
+      if (token.ttype != tokens.DOT) {
+        PascalErrorHandler.flag(token, error.MISSING_PERIOD, this)
+      }
 
-          // append the current line number to the entry
-          entry.appendLineNumber(token.lineNumber)
-        } else if (token.ttype == tokens.ERROR) {
-          PascalErrorHandler.flag(token, token.tvalue.asInstanceOf[PascalErrorCode], this)
-        }
-        token = nextToken()
+      token = scanner.currentToken
+      if (root != null)
+      {
+        icode.setRoot(root)
       }
 
       val elapsedTime = (System.currentTimeMillis - start)/1000.0f
@@ -40,6 +44,6 @@ class PascalParser(scanner: PascalScanner) extends Parser(scanner) {
     } catch {
       case _: Throwable => PascalErrorHandler.abortTranslation(error.IO_ERROR, this)
     }
-    (null, symTabStack)
+    (icode, symTabStack)
   }
 }
