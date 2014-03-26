674c55bf08d42c6741d1798d040d8c64cc4561cd
John <jfourkiotis@gmail.com>
2014-03-26 19:25:35 +0200

    CompoundStatementParser

--- src/main/scala/ice/pascal/frontend/CompoundStatementParser.scala
+++ src/main/scala/ice/pascal/frontend/CompoundStatementParser.scala
@@ -1,13 +1,34 @@
 package ice.pascal.frontend
 
-import ice.message.MessageProducer
+import ice.message.{MessageListener, MessageProducer}
 import ice.frontend.Token
-import ice.intermediate.ICodeNode
+import ice.intermediate.{ICodeNodeType, ICode, SymTabStack, ICodeNode}
 
 /**
  * Created by john on 3/19/14.
  */
-class CompoundStatementParser extends MessageProducer {
-  def parse(token: Token): ICodeNode = ???
+class CompoundStatementParser(scanner: PascalScanner, symtabstack: SymTabStack) extends MessageProducer {
+  def parse(token: Token): ICodeNode = {
+    var tok = token
+    require(tok.ttype == tokens.BEGIN)
 
+    // consume BEGIN
+    tok = scanner.nextToken()
+
+    // create COMPOUND node.
+    val compound = ICode.createICodeNode(ICodeNodeType.COMPOUND)
+
+    // parse the statement list terminated by the END token.
+    val statement_parser = StatementParser(scanner, symtabstack, listeners)
+    statement_parser.parseList(tok, compound, tokens.END, error.MISSING_END)
+    compound
+  }
+}
+
+object CompoundStatementParser {
+  def apply(scanner: PascalScanner, symtabstack: SymTabStack, listeners: Seq[MessageListener]) = {
+    val cp = new CompoundStatementParser(scanner, symtabstack)
+    cp.listeners ++= listeners
+    cp
+  }
 }
